0a8a8c38a880e44496a94ccc978454988415e5f1
Antti Lahtinen <antlaht86@gmail.com>
2018-07-09 21:33:37 +0300

    1_5

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"log"
 	"fmt"
+	"database/sql"
 )
 
 func determineListenAddress() (string, error) {
@@ -16,18 +17,54 @@ func determineListenAddress() (string, error) {
 	return ":" + port, nil
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World")
+type Book struct {
+	isbn   string
+	title  string
+	author string
+	price  float32
 }
-
 func main() {
 	addr, err := determineListenAddress()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", hello)
-	log.Printf("Listening on %s...\n", addr)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You connected to your database.")
+
+	rows, err := db.Query("SELECT * FROM books;")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	bks := make([]Book, 0)
+	for rows.Next() {
+		bk := Book{}
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price) // order matters
+		if err != nil {
+			panic(err)
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+
+	for _, bk := range bks {
+		// fmt.Println(bk.isbn, bk.title, bk.author, bk.price)
+		fmt.Printf("%s, %s, %s, $%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+	}
+
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
